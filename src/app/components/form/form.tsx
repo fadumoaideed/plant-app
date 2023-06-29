1fff373893e4e2ac56671477d14f2042e1aa356a
fadumoaideed <fadumo.ahmed.aideed@gmail.com>
2023-06-29 11:44:52 +0100

    basic form field

--- src/app/components/form/form.tsx
+++ src/app/components/form/form.tsx
@@ -5,19 +5,19 @@ interface FormInputInterface {
   plantname?: string;
 }
 function Form() {
-  const [form, setForm] = useState<FormInputInterface>({});
+  const [plantname, setPlantname] = useState('');
   function handleSubmit(e: any) {
     e.preventDefault();
-    alert(form);
+    alert(plantname);
   }
   function handleChange(e: any) {
-    setForm({ ...form, plantname: e.target.value });
+    setPlantname(e.target.value);
   }
   return (
     <div>
       <form onSubmit={handleSubmit}></form>
       <h3>Plant name</h3>
-      <input type="text" name="plantname" onChange={handleChange} value={form.plantname} />
+      <input type="text" name="plantname" onChange={handleChange} value={plantname} />
       <input type="submit" />
     </div>
   );
